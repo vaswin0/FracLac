53b7c6082f9c0ae9093568b016972ca5404dd153
vaswin0 <vaswin0137@gmail.com>
2023-06-13 19:10:27 +0530

    count fn

--- fracDim.cpp
+++ fracDim.cpp
@@ -4,10 +4,52 @@
 using namespace std;
 
 
+int countOccupied(Matrix &x, int box){
+
+	int count = 0;
+
+	for(int i = 0; i < x.numRows - box + 1; i+= box){
+		
+		for(int j = 0; j < x.numCols - box + 1; j += box){
+			
+			int sum = 0;
+			for( int k = i; k < i + box; k++){
+
+				for(int l = j; l < j + box; l++){
+
+					sum += x._(k,l);
+
+					}
+
+				}
+
+				if(sum != 0) count+= 1;
+
+			}
+
+
+		}
+
+	return count;
+
+	}
+
+			
+
+
+
+
+
+
+
+
+
+
 int main(){
 
-Matrix a(4,4) ;
-
+Matrix a(5,5) ;
+a.initMatrix();
+cout << countOccupied(a,2) << endl;
 return 0;
 
 }
